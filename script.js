0b20122f09c7b133dc1fe16ad8c44b9d6183cf4c
Fredy Asprilla <93607647+FredyAsprilla@users.noreply.github.com>
2023-02-26 18:08:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('p1').addEventListener('click', ()=> {
+    document.body.style.background = 'Yellow';
+})
+
+document.querySelector('p2').addEventListener('click', ()=> {
+    document.body.style.background = 'blue';
+})
+
+document.querySelector('p3').addEventListener('click', ()=> {
+    document.body.style.background = 'red';
+})
